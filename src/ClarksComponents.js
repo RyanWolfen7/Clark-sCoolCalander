1e80ffbf9a2412c94985e8064c9595bc043d356e
ryanwolfen7 <wolfen7lada@gmail.com>
2019-11-04 21:31:27 +0000

    created and implimented clark section

--- src/ClarksComponents.js
+++ src/ClarksComponents.js
@@ -1,2 +1,9 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+
+export const ClarkSection = styled.section`
+&& {
+    max-width: 45rem;
+    margin: auto;
+}
+`
--- src/components/DeliveryDateSection/DeliveryDateSection.js
+++ src/components/DeliveryDateSection/DeliveryDateSection.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { ClarkSection } from '../../ClarksComponents'
 
 const DeliveryDateSection = (props) => {
 
     return (
-        <section class='delivery-date-section'>
+        <ClarkSection>
             <div class='title-contaier'>
                 <h2>
                     "Choose your delivery day"
                     <span class='title-container__free-delivery-notice'> Delivery is always free</span>
                 </h2>
             </div>
-        </section>
+        </ClarkSection>
     )
 }
 
