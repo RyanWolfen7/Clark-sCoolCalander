050220bf0794c2b02017422ed078b2c00b23b672
ryanwolfen7 <wolfen7lada@gmail.com>
2019-11-04 21:22:25 +0000

    made a js styled component

--- src/components/DeliveryDateSection/DeliveryDateSection.js
+++ src/components/DeliveryDateSection/DeliveryDateSection.js
@@ -4,7 +4,12 @@ const DeliveryDateSection = (props) => {
 
     return (
         <section class='delivery-date-section'>
-
+            <div class='title-contaier'>
+                <h2>
+                    "Choose your delivery day"
+                    <span class='title-container__free-delivery-notice'> Delivery is always free</span>
+                </h2>
+            </div>
         </section>
     )
 }
